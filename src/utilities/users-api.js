4117788004ad8db79740ce53ed530fe08ba67625
Esser, Mishelle <EsserMishelle@gmail.com>
2024-03-06 11:43:37 -0500

    frontend

--- src/utilities/users-api.js
+++ src/utilities/users-api.js
@@ -1,12 +1,11 @@
+import sendRequest from "./send-request";
 
-import sendRequest from './send-request';
-
-const BASE_URL = '/api/users';
+const BASE_URL = "https://restaurant-app-back.onrender.com/api/users";
 
 export function signUp(userData) {
-  return sendRequest(BASE_URL, 'POST', userData);
+  return sendRequest(BASE_URL, "POST", userData);
 }
 
 export function logIn(credentials) {
-  return sendRequest(`${BASE_URL}/login`, 'POST', credentials);
+  return sendRequest(`${BASE_URL}/login`, "POST", credentials);
 }
