cf435763ae7ca4415391bc707abfc4887c4bfcda
Esser, Mishelle <EsserMishelle@gmail.com>
2024-03-08 12:23:41 -0500

    backend url in send-request

--- src/utilities/send-request.js
+++ src/utilities/send-request.js
@@ -18,7 +18,7 @@ export default async function sendRequest(url, method = "GET", payload = null) {
     // Prefacing with 'Bearer' is recommended in the HTTP specification
     options.headers.Authorization = `Bearer ${token}`;
   }
-
+  const url = "https://restaurant-app-back.onrender.com";
   // const res = await fetch(url);
   const res = await fetch(url, options);
   // res.ok will be false if the status code set to 4xx in the controller action
